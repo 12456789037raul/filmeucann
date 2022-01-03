4b3d4691165acd58a1e0b708b61f72694d97ddbf
12456789037raul <97033029+12456789037raul@users.noreply.github.com>
2022-01-03 23:55:45 +0100

    fileDatabaseAdd

---
+++ src/filePessoa/Read.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fileDatabase;
+
+import GenericObject.GenericObject;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author vicente-jpro
+ */
+public class Read {
+    
+    FileInputStream entradaArquivo;
+
+    public Read(String path) throws FileNotFoundException {
+        this.entradaArquivo = new FileInputStream( new File(path));
+    }
+    
+    public List<GenericObject> readNow(){
+        
+        Scanner lerArquivo = new Scanner(entradaArquivo, "UTF-8");
+      
+        List lista = new ArrayList<GenericObject>();
+        
+        while(lerArquivo.hasNext()){
+            Object nomeNoArquivo = lerArquivo.nextLine();
+            lista.add( nomeNoArquivo );
+           
+        }
+        
+        return lista;
+    }
+    
+  
+    
+}
---
+++ src/fileProvincia/Delete.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fileDatabase;
+
+import GenericObject.GenericObject;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ *
+ * @author vicente-jpro
+ */
+public class Delete {
+     private File arquivo;
+     private GenericObject information;
+     private String path;
+     private final String MUDAR_LINHA = "\n";
+     
+    /**
+     *
+     * @param path src/fileDatabase/nomeDoFicheiro.txt
+     */
+    public Delete(String path, GenericObject information){
+         this.information = information;
+         this.path = path;
+         this.arquivo = new File(path);
+     }
+    
+    public void deleteNow() throws IOException{
+        createFile();
+    }
+    // Cria um novo ficheiro
+    private void createFile() throws IOException{
+    
+        if (!arquivo.exists() ){
+              Object pathArquivo = arquivo.createNewFile();
+              System.out.println(""+pathArquivo);
+           
+        }
+        
+        
+        int posicaoElemento = existElemento( information );
+        if ( posicaoElemento == -1 )
+        {
+            System.out.println("Elemento não exite no ficheiro");
+        }else{
+                // Elemento existente
+            Read elementos = new Read(path);
+            List<GenericObject> lista = elementos.readNow();
+            lista.remove(posicaoElemento);
+            
+
+            FileWriter write = new FileWriter(this.arquivo);
+
+            for( int i = 0; i <lista.size(); i++ ){
+
+                String dadoDaLista = lista.get(i).toString();
+                write.write(dadoDaLista+""+MUDAR_LINHA);
+
+            }
+            write.flush();
+            write.close();
+            
+        }
+            
+        
+        
+    }      
+  
+    // Retorna  a posição de um elemento existente na lista
+    private int existElemento( Object elemento) throws FileNotFoundException{
+        Read elementos = new Read(path);
+        List<GenericObject> lista = elementos.readNow();
+        
+        int posicao = 0;
+        for ( int i = 0; i < lista.size(); i++ )
+        {
+            
+
+            String elementoLista = lista.get(i).toString();
+            if ( elementoLista.equalsIgnoreCase(elemento.toString()) ){
+                return i;
+             }
+        }
+        
+        return -1;
+    }
+        
+        
+}
