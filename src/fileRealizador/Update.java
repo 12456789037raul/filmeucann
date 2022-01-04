23b426b858e7536d3cb99fc09f78be6ff02aca27
12456789037raul <97033029+12456789037raul@users.noreply.github.com>
2022-01-04 03:03:12 +0100

    ghcghj

--- src/fileRealizador/Update.java
+++ src/fileRealizador/Update.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package fileDatabase;
+package fileUpdate;
 
-import GenericObject.GenericObject;
+//import GenericObject.GenericObject;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
